10f7a3f18b74fb475ff6c313037e77b907f361bc
Natalia <72054931+NatalyMTZ@users.noreply.github.com>
2024-06-21 01:25:12 -0600

    Update LoginController.java

--- Proyecto Final/Interfaz/biblioteca/biblioteca/src/LoginController.java
+++ Proyecto Final/Interfaz/biblioteca/biblioteca/src/LoginController.java
@@ -1,7 +1,14 @@
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -14,4 +21,17 @@ public class LoginController {
     @FXML
     private TextField tfUsuario;
 
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Parent catalogoUserParent = FXMLLoader.load(getClass().getResource("CatalogoUser.fxml"));
+        Scene catalogoUserScene = new Scene(catalogoUserParent);
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        
+        appStage.setScene(catalogoUserScene);
+        
+        // Centrar la ventana
+        appStage.centerOnScreen();
+        
+        appStage.show();
+    }
 }
